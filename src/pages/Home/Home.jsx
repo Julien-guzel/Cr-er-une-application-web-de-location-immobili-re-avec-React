ce9bbeb031fcafbdaa3e28aa519a2f4aa9d74a31
Julien-guzel <irfanguzel1997@gmail.com>
2023-02-14 02:29:38 +0100

    first

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,40 @@
+import { useEffect, useState } from "react";
+import Banner from "../../components/Banner/Banner";
+import Card from "../../components/Cards/Card";
+import { Link } from "react-router-dom";
+import axios from "axios";
+
+export default function Home() {
+
+
+	// useState est un tableau
+	const [data, setData] = useState([]);
+
+
+
+	useEffect(() => {
+		axios.get("/logements.json")
+		.then((res) => setData(res.data)); //requète AXIOS ici également pour prochaine utilisation API
+	}, []);
+
+
+
+	return (
+		<>
+			<Banner />
+			<div className="cards-container">
+				{data.map((appart, id) => (
+					<div className="card_logement" key={id}>
+						<Link className="link_card_logement" to={`/logement/${appart.id}`}>
+							<Card cover={appart.cover} title={appart.title} />
+						</Link>
+					</div>
+				))}
+			</div>
+		</>
+	);
+
+
+
+
+}
